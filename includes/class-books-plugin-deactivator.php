f4bd6b1ed56691ed33a4de666da91e1e524d8d19
alireza abedi <a.alireza1347@gmail.com>
2024-07-15 02:31:33 +0330

    some changes

--- includes/class-books-plugin-deactivator.php
+++ includes/class-books-plugin-deactivator.php
@@ -3,11 +3,11 @@
 namespace BooksPlugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly.
+    exit;
 }
 
 class Books_Plugin_Deactivator {
     public static function deactivate() {
-        // We typically do not drop tables on deactivate to avoid data loss.
+        // We do not drop tables on deactivate
     }
 }
